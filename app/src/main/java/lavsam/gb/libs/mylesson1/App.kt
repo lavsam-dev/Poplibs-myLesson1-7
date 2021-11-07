bf4bb82ec8787433fc86d3dd784b01b6aa28d933
lavsam-dev <lavsam@gmail.com>
2021-11-07 17:21:38 +0400

    App

--- app/src/main/java/lavsam/gb/libs/mylesson1/App.kt
+++ app/src/main/java/lavsam/gb/libs/mylesson1/App.kt
@@ -1,9 +1,13 @@
 package lavsam.gb.libs.mylesson1
 
 import android.app.Application
+import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.github.terrakok.cicerone.Cicerone
 import com.github.terrakok.cicerone.Router
-
+import lavsam.gb.libs.mylesson1.database.AppDatabase
+const val ROOM_NAME = "poplibs.db"
 class App : Application() {
 
     companion object {
@@ -17,9 +21,27 @@ class App : Application() {
 
     val navigatorHolder get() = cicerone.getNavigatorHolder()
     val router get() = cicerone.router
+    val repository = GithubUsersRepo()
+    private lateinit var db: AppDatabase
+
+    fun getDB(): AppDatabase {
+        return db
+    }
 
     override fun onCreate() {
         super.onCreate()
         instance = this
+        val value = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE User ADD COLUMN age INTEGER DEFAULT 0 NOT NULL")
+            }
+        }
+        db = Room.databaseBuilder(
+            this,
+            AppDatabase::class.java,
+            ROOM_NAME
+        )
+            .addMigrations(value)
+            .build()
     }
 }
